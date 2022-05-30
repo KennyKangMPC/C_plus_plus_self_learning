5130b858a7660acb3e066bc13d165c82416291c4
Kangqi Fu <kfu9@wisc.edu>
2022-05-30 21:37:21 +0800

    create ch6 notes

---
+++ ch6_class/Book.hpp
@@ -0,0 +1,8 @@
+#include <string>
+
+class Book {
+    public:
+        std::string author, title, publisher, format;
+        int price; // Given in Pence
+        int yearOfPublication;
+}; //Note that the class ends with ;
