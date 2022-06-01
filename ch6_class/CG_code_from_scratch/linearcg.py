dc96bee6114774840ec3d11da895be68a589479d
Kangqi Fu <kfu9@wisc.edu>
2022-06-01 22:03:14 +0800

    create files for conjugate gradient tutorials

---
+++ ch6_class/CG_code_from_scratch/linearcg.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from itertools import product
+from warnings import warn
+from sklearn.datasets import make_spd_matrix
+
+def LinearCG(A, b, x0, tol=1e-5):
+    xk = x0
